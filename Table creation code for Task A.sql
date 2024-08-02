953258d723500f92b2fe32577b72aae90d08f3ee
Ayodele Adeniyi <dexithub@gmail.com>
2024-08-02 14:06:01 +0100

    Update Table creation code for Task A.sql

--- Table creation code for Task A.sql
+++ Table creation code for Task A.sql
@@ -21,8 +21,7 @@ CREATE TABLE Dim_Vehicle (
     vin VARCHAR(50),
     reg_number VARCHAR(50),
     mileage INT,
-    customer_id INT,
-    FOREIGN KEY (customer_id) REFERENCES Dim_Customer(customer_id)
+    customer_id INT
 );
 GO
 
@@ -33,8 +32,7 @@ CREATE TABLE Dim_Service (
     hours DECIMAL(5,2),
     rate DECIMAL(10,2),
     amount DECIMAL(10,2),
-    vehicle_id INT,
-    FOREIGN KEY (vehicle_id) REFERENCES Dim_Vehicle(vehicle_id)
+    vehicle_id INT
 );
 GO
 
@@ -57,6 +55,23 @@ CREATE TABLE Dim_Location (
 );
 GO
 
+--Create Subtotal Dimension Table
+CREATE TABLE Dim_Subtotals (
+	subtotal_id INT IDENTITY(1,1) PRIMARY KEY,
+	customer_id INT,
+	Part_subtotal DECIMAL(10,2),
+	Service_subtotal DECIMAL(10,2)
+);
+GO
+
+--Create Date Dimension Table
+CREATE TABLE Dim_Date (
+	date_id INT IDENTITY(1,1) PRIMARY KEY,
+	date_issued DATE,
+	due_date DATE
+);
+GO
+
 -- Create Fact Table for Invoice
 CREATE TABLE Fact_Invoice (
     invoice_id INT IDENTITY(1,1) PRIMARY KEY,
@@ -65,8 +80,8 @@ CREATE TABLE Fact_Invoice (
     service_id INT,
     part_id INT,
     location_id INT,
-    date_issued DATE,
-    due_date DATE,
+	subtotal_id INT,
+	date_id INT,
     total_service_charge DECIMAL(10,2),
     total_part_charge DECIMAL(10,2),
     sub_total DECIMAL(10,2),
@@ -77,6 +92,8 @@ CREATE TABLE Fact_Invoice (
     FOREIGN KEY (vehicle_id) REFERENCES Dim_Vehicle(vehicle_id),
     FOREIGN KEY (service_id) REFERENCES Dim_Service(service_id),
     FOREIGN KEY (part_id) REFERENCES Dim_Part(part_id),
-    FOREIGN KEY (location_id) REFERENCES Dim_Location(location_id)
+    FOREIGN KEY (location_id) REFERENCES Dim_Location(location_id),
+	FOREIGN KEY (subtotal_id) REFERENCES Dim_Subtotals(subtotal_id),
+	FOREIGN KEY (date_id) REFERENCES Dim_Date(date_id)
 );
 GO
